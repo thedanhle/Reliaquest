087673566f67c30e2b3d2c700fe0d35c969dc810
Danh Le <danh.le.engineer@gmail.com>
2025-02-10 22:57:28 -0500

    adding controller

--- api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
+++ api/src/main/java/com/reliaquest/api/controller/EmployeeController.java
@@ -1,4 +1,66 @@
 package com.reliaquest.api.controller;
 
-public class EmployeeController {
+import com.reliaquest.api.model.Employee;
+import com.reliaquest.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/employee")
+public class EmployeeController implements IEmployeeController<Employee, Employee> {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @Override
+    @GetMapping("/search")
+    public ResponseEntity<List<Employee>> getEmployeesByNameSearch(@PathVariable String searchString) {
+        return ResponseEntity.ok(employeeService.getEmployeesByNameSearch(searchString));
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable String id) {
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    @Override
+    @GetMapping("/highestSalary")
+    public ResponseEntity<Integer> getHighestSalaryOfEmployees() {
+        return ResponseEntity.ok(employeeService.getHighestSalaryOfEmployees());
+    }
+
+    @Override
+    @GetMapping("/top10salaries")
+    public ResponseEntity<List<String>> getTopTenHighestEarningEmployeeNames() {
+        return ResponseEntity.ok(employeeService.getTop10HighestEarningEmployeeNames());
+    }
+
+    @Override
+    @PostMapping()
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employeeInput) {
+        return ResponseEntity.ok(employeeService.createEmployee(employeeInput));
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployeeById(@PathVariable String id) {
+        return ResponseEntity.ok(employeeService.deleteEmployeeById(id));
+    }
 }
